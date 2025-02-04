43c1170de014acb8cea9e949146b189485627f9f
Vishal0504-tech <vrvishal0504@gmail.com>
2025-02-04 12:20:22 +0530

    Update Services.tsx

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -3,36 +3,39 @@ import Image from 'next/image';
 
 function Services() {
   return (
-    <div className='container mx-auto my-8 border-b pb-10'>
-        <h2 className='mb-20 text-center text-3xl tracking-tighter sm:text-4xl lg:text-5xl'>
-            Services
-        </h2>
-        <div className='flex flex-wrap'>
-            <div className='w-full lg:w-1/2'>
-                <div className='p-6'>
-                    <Image
-                    className='rounded-xl object-cover p-5'
-                    src="/service.jpeg"
-                    width={600}
-                    height={600}
-                    
-                    alt='services'
-                    />
-                </div>
-            </div>
-            <div className='w-full lg:w-1/2 grid-cols-1'>
-                <h2 className='mt-10 text-5xl lg:text-7xl'>
-                    We are 
-                     <span> available in</span>
-                    <span>40+ Countries!</span>
-                </h2>
-                <p className='max-w-full py-6 text-lg tracking-tighter lg:pr-20'>
-                    {SERVICES_TEXT}
-                </p>
-            </div>
+    <div className='container mx-auto my-8 border-b pb-10 px-4'>
+      <h2 className='mb-12 text-center text-3xl tracking-tighter sm:text-4xl lg:text-5xl'>
+        Services
+      </h2>
+
+      <div className='flex flex-wrap items-center justify-center'>
+         
+        <div className='w-full lg:w-1/2 flex justify-center'>
+          <div className='p-4'>
+            <Image
+              className='rounded-xl object-cover'
+              src="/service.jpeg"
+              width={500}
+              height={500}
+              alt='services'
+            />
+          </div>
         </div>
+
+         
+        <div className='w-full lg:w-1/2 flex flex-col items-center lg:items-start text-center lg:text-left'>
+          <h2 className='mt-6 text-4xl sm:text-5xl lg:text-6xl font-semibold'>
+            We are <br />
+            <span className="text-blue-600">available in</span> <br />
+            <span className="text-green-600">40+ Countries!</span>
+          </h2>
+          <p className='mt-4 max-w-lg text-lg tracking-tighter lg:pr-10'>
+            {SERVICES_TEXT}
+          </p>
+        </div>
+      </div>
     </div>
   )
 }
 
-export default Services
+export default Services;
